d6aa50a06b29817485b978699fc9d87c44a9dedd
William Donovan-Seid <wdstwo@live.com>
2020-10-23 10:44:11 -0700

    add splash page that lists all flavors and treats

--- PierreMarket/Controllers/HomeController.cs
+++ PierreMarket/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using PierreMarket.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PierreMarket.Controllers
 {
@@ -9,6 +12,22 @@ namespace PierreMarket.Controllers
     public ActionResult Index()
     {
       return View();
+    }
+
+    private readonly PierreMarketContext _db;
+    public HomeController(PierreMarketContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult AllFlavAndTreat()
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Flavor> allFlav = _db.Flavors.OrderBy(x => x.FlavorName).ToList();
+      List<Treat> allTreat = _db.Treats.OrderBy(x => x.TreatName).ToList();
+      model.Add("flavors", allFlav);
+      model.Add("treats", allTreat);
+      return View(model);
     }
 
   }
